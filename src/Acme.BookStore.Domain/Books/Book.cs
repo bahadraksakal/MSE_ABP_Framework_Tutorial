6d7d646f9efe9ed87e68fb5877b8563d5f426564
Bahadır Aksakal <72493865+bahadraksakal@users.noreply.github.com>
2023-11-13 12:04:57 +0300

    MSE_ABP_Framework_Tutorial Part 1

---
+++ src/Acme.BookStore.Domain/Books/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Books
+{
+    // Son değiştirilme tarihi vb... otomatik ekler. 
+    public class Book : AuditedAggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+        public BookType Type { get; set; }
+        public DateTime PublishDate { get; set; }
+        public float Price { get; set; }
+    }
+}
